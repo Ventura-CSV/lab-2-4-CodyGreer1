ae7a2ca3fcc8d23bd79c25f346cec7cb5bb87f90
CodyGreer1 <143456594+CodyGreer1@users.noreply.github.com>
2023-08-30 14:52:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     ##################################################
-    # Comlete your code here
+    original.str="Python Programming"
+        sub1=original.str[Python]
+        sub2=original.str[Programming]
+merge_str=sub2+sub1
+    print(sub2)
+    print(sub1)
+    print(merge_str)
     ##################################################
     pass
 
